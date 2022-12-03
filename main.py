696964c6a2dfedf8a747304b33d9de5e12389d43
cello cello <119573100+VyuzlaStyle@users.noreply.github.com>
2022-12-03 08:31:21 +0000

    except избирательный

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ if __name__ == '__main__':
   try:
     token = read_token()
   except KeyError as e:
-    print("Error no token in system enviroment")
+    print(f"Error: {e} \n no token in system enviroment")
     sys.exit()
 
   link_for_check = input("Enter your link: ")
-  
-  if bit_ly.is_bitlink(token, link_for_check):
-    print(bit_ly.count_clicks(token, link_for_check))
-  else:
-    print(f"Битлинк {bit_ly.get_short_link(token,link_for_check)}")
-  except:
-    print(f"Error: your link <{link_for_check}> is incorrect ")
+  try:
+    if bit_ly.is_bitlink(token, link_for_check):
+      print(bit_ly.count_clicks(token, link_for_check))
+    else:
+      print(f"Битлинк {bit_ly.get_short_link(token,link_for_check)}")
+  except requests.exceptions.HTTPError as e:
+    print(f"Error:{e} \n your link <{link_for_check}> is incorrect \n")
     sys.exit()
